f8902194e71e9ff4bb7f9b1eb25b0ca8c3fade8e
wail madrane <wail.madrane@gmail.com>
2025-11-26 19:20:16 +0100

    Challenge 002 - add

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,12 @@
 export function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
 export function add2(...param1: number[]): number {
-
+  return param1.reduce((acc, cur) => acc + cur, 0);
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
