8b8474777bc5db9f12a4b24120624ea6953091f3
wail madrane <wail.madrane@gmail.com>
2025-11-26 19:12:12 +0100

    Challenge 001 - absoluteValuesSumMinization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,10 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    if (!a || a.length === 0) throw new Error("error: input need to a least contain 1 element.");
+    if (a.length === 1) return a[0];
+    return a[Math.floor((a.length - 1)/2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
